43e31b92cff3464b10d37c3a246a7152af7531b4
megalodondestructor <daw10.2021@gmail.com>
2021-03-11 22:30:54 +0100

    implementacion del test Testpersona

---
+++ src/ed/examen/modelo/PersonaTest.java
@@ -0,0 +1,50 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+	private Persona oscar = new Persona("11111111L", "oscar", "garcia");
+
+	@Test
+	void testPersona() {
+		assertEquals("11111111L", oscar.getDni());
+	}
+
+	@Test
+	void testPersonaStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
