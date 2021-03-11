292c69c52fae3c4540bd7571fbf0b4183bcd30b3
megalodondestructor <daw10.2021@gmail.com>
2021-03-11 22:22:42 +0100

    implementacion del test anadiralumnotest

---
+++ src/ed/examen/modelo/CursoTest.java
@@ -0,0 +1,56 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+	private Curso micurso = new Curso();
+	@Test
+	public void eliminarAlumnoTest() throws Exception {
+
+		Persona oscar = new Persona("11111111L", "Oscar", "Garcia");
+		micurso.aniadirAlumno(oscar);
+
+		Integer Numerodealumnosantes = micurso.numeroAlumnos();
+
+		micurso.eliminarAlumno(oscar.getDni());
+
+		assertEquals(Numerodealumnosantes.intValue() - 1, micurso.numeroAlumnos().intValue());
+		assertFalse(micurso.estaRegistrado(oscar.getDni()));
+
+	}
+
+	@Test
+	public void aniadirAlumnoTest() {
+		Integer Numerodealumnosantes = micurso.numeroAlumnos();
+
+		Persona oscar = new Persona("11111111L", "Oscar", "Garcia");
+		micurso.aniadirAlumno(oscar);
+
+		assertEquals(Numerodealumnosantes.intValue() + 1, micurso.numeroAlumnos().intValue());
+		assertTrue(micurso.estaRegistrado(oscar.getDni()));
+
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarTodos() {
+		fail("Not yet implemented");
+	}
+
+}
